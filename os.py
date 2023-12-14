3f1996bde4e556426643aaf7f95b1e277dce5f62
nariman mirbod <narimanpc1382@gmail.com>
2023-12-15 00:21:51 +0330

    os ruls

--- os.py
+++ os.py
@@ -1,7 +1,18 @@
 import platform
-
-# دریافت اطلاعات مربوط به سیستم عامل
+import subprocess
+host = "194.33.105.38"
 system_info = platform.system()
-
-# چاپ نام سیستم عامل
-print(f"نام سیستم عامل: {system_info}")
+command = "sudo apt-get update"
+command2 = f"ping {host}"
+if system_info == "linux":
+    try:
+        result =  subprocess.run(['sudo', '-S'] + command.split(), check=True, text=True, input='dev2256N\n')
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"error : {e}")
+elif system_info == "Windows":
+    try:
+        result = subprocess.run(command2, capture_output=True, text=True, shell=True, check=True)
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"error : {e}")
