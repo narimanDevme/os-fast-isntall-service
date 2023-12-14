b1b192eb434ca723dcb21fa74979cc2d20241973
nariman mirbod <narimanpc1382@gmail.com>
2023-12-15 01:17:06 +0330

    debug 2

--- os.py
+++ os.py
@@ -5,10 +5,9 @@ host = "194.33.105.38"
 system_info = platform.system()
 
 l_command1 = "apt-get update\n"
-l_command2 = "curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh -y\nsh setup-repos.sh -y"
+l_command2 = "curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh"
+l_command4 = "sh setup-repos.sh"
 l_command3 = "apt-get install webmin --install-recommends -y\n"
-
-
 command2 = f"ping {host}"
 if system_info == "Linux":
     try:
@@ -16,9 +15,11 @@ if system_info == "Linux":
         print(result1.stdout)
         time.sleep(1)
 
-        result2 = subprocess.run(['sudo', '-S'] + l_command2.split(), check=True, text=True, input='dev2256N\n')
+        result2 = subprocess.run(l_command2.split(), check=True, text=True, input='dev2256N\n')
         print(result2)
-        result3 = subprocess.run(['sudo', '-S'] + l_command3.split(), check=True, text=True, input='dev2256N\n')
+        result4 = subprocess.run(l_command4.split(), check=True, text=True, input='dev2256N\n')
+        print(result4)
+        result3 = subprocess.run(l_command3.split(), check=True, text=True, input='dev2256N\n')
         print(result3)
     except subprocess.CalledProcessError as e:
         print(f"error : {e}")
