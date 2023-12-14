398daa41996eafb807021dd84ca4ced992e24973
nariman mirbod <narimanpc1382@gmail.com>
2023-12-15 01:39:54 +0330

    loading vsftpd

--- os.py
+++ os.py
@@ -8,19 +8,36 @@ l_command1 = "apt-get update\n"
 l_command2 = "curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh"
 l_command4 = "sh setup-repos.sh"
 l_command3 = "apt-get install webmin --install-recommends -y\n"
+l_command5 = "apt-get install vsftpd -y"
+l_command6 = "systemctl start vsftpd"
+l_command7 = "systemctl enable vsftpd"
 command2 = f"ping {host}"
 if system_info == "Linux":
     try:
+
+        # update
         result1 =  subprocess.run(['sudo', '-S'] + l_command1.split(), check=True, text=True, input='dev2256N\n')
         print(result1.stdout)
         time.sleep(1)
 
+        #webmin
         result2 = subprocess.run(l_command2.split(), check=True, text=True, input='dev2256N\n')
         print(result2)
         result4 = subprocess.run(l_command4.split(), check=True, text=True, input='dev2256N\n')
         print(result4)
         result3 = subprocess.run(l_command3.split(), check=True, text=True, input='dev2256N\n')
         print(result3)
+
+        #vsftpd
+        result5 = subprocess.run(l_command5.split(), check=True, text=True, input='dev2256N\n')
+        print(result5)
+        result6 = subprocess.run(l_command6.split(), check=True, text=True, input='dev2256N\n')
+        print(result6)
+        result7 = subprocess.run(l_command7.split(), check=True, text=True, input='dev2256N\n')
+        print(result7)
+        
+
+
     except subprocess.CalledProcessError as e:
         print(f"error : {e}")
 elif system_info == "Windows":
