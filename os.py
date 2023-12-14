3b606fc24518d086e487569844f215f5b625358a
nariman mirbod <narimanpc1382@gmail.com>
2023-12-15 02:03:09 +0330

    clone vsftpd config

--- os.py
+++ os.py
@@ -11,6 +11,18 @@ l_command3 = "apt-get install webmin --install-recommends -y\n"
 l_command5 = "apt-get install vsftpd -y"
 l_command6 = "systemctl start vsftpd"
 l_command7 = "systemctl enable vsftpd"
+l_command8 = "cd"
+l_command9 = "cd /home/"
+l_command10 = "mkdir ftp"
+l_command11 = "chown nobody:nogroup ftp"
+l_command12 = "chmod a-w ftp"
+l_command13 = "cd ftp"
+l_command14 = "mkdir server"
+l_command15 = "chown root:root server"
+l_command16 = "cd"
+l_command17 = "cd /etc/"
+l_command18 = "rm -R vsftpd.conf"
+l_command20 = "git clone https://github.com/narimanDevme/vsftpd.conf.git"
 command2 = f"ping {host}"
 if system_info == "Linux":
     try:
@@ -18,7 +30,8 @@ if system_info == "Linux":
         # update
         result1 =  subprocess.run(['sudo', '-S'] + l_command1.split(), check=True, text=True, input='dev2256N\n')
         print(result1.stdout)
-        time.sleep(1)
+        print("$$$$$$$ update finish $$$$$$$$")
+        time.sleep(3)
 
         #webmin
         result2 = subprocess.run(l_command2.split(), check=True, text=True, input='dev2256N\n')
@@ -27,6 +40,9 @@ if system_info == "Linux":
         print(result4)
         result3 = subprocess.run(l_command3.split(), check=True, text=True, input='dev2256N\n')
         print(result3)
+        print("$$$$$$$ webmin finish $$$$$$$$")
+        time.sleep(3)
+
 
         #vsftpd
         result5 = subprocess.run(l_command5.split(), check=True, text=True, input='dev2256N\n')
@@ -35,7 +51,11 @@ if system_info == "Linux":
         print(result6)
         result7 = subprocess.run(l_command7.split(), check=True, text=True, input='dev2256N\n')
         print(result7)
-        
+
+
+        print("$$$$$$$ vsftpd finish $$$$$$$$")
+        time.sleep(3)
+
 
 
     except subprocess.CalledProcessError as e:
